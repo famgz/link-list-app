32296c0790cb6073411395adc33f13c2f5056963
famgz <famgz@proton.me>
2024-01-10 10:44:40 -0300

    feat: home page

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,17 +1,25 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import './globals.css';
+import Header from '@/components/Header';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Link List App',
   description: 'Linktree clone created with NextJS',
-}
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang='en'>
+      <body className={inter.className}>
+        <main>
+          <Header />
+          <div className='max-w-4xl mx-auto p-6'>
+          {children}
+          </div>
+        </main>
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,5 +1,29 @@
 export default function Home() {
   return (
-    <div>test</div>
-  )
+    <main>
+      <section className='pt-32'>
+        <div className='max-w-md mb-8'>
+          <h1 className='text-6xl font-bold'>
+            Your single link <br /> for everything
+          </h1>
+          <h2 className='text-slate-400 text-xl mt-6'>
+            Share your links, social profiles, contact info and more on one page
+          </h2>
+        </div>
+        <form className='inline-flex items-center shadow-lg shadow-gray-700/30'>
+          <span className='bg-white py-4 pl-4'>linklist.to/</span>
+          <input
+            type='text'
+            name=''
+            id=''
+            className='py-4'
+            placeholder='username'
+          />
+          <button type='submit' className='bg-blue-500 text-white py-4 px-6'>
+            Join for Free
+          </button>
+        </form>
+      </section>
+    </main>
+  );
 }
