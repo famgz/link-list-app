a1727858134d973ae264dc573bc1c023136bb472
famgz <famgz@proton.me>
2024-01-26 09:27:37 -0300

    fix: set max width for main account wrapper

--- src/components/layout/SectionBox.jsx
+++ src/components/layout/SectionBox.jsx
@@ -1,3 +1,7 @@
 export default function SectionBox({ children }) {
-  return <div className='bg-white m-8 p-4 shadow-lg'>{children}</div>;
+  return (
+    <div className='bg-white m-8 p-4 shadow-lg max-w-5xl min-w-[300px] mx-auto'>
+      {children}
+    </div>
+  );
 }
