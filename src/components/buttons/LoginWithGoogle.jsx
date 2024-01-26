505099144c54649c8075bc2990f132550bfcb1ed
famgz <famgz@proton.me>
2024-01-26 09:26:30 -0300

    fix: google signin callback url

--- src/components/buttons/LoginWithGoogle.jsx
+++ src/components/buttons/LoginWithGoogle.jsx
@@ -7,7 +7,7 @@ import { signIn } from 'next-auth/react';
 export default function LoginWithGoogle(params) {
   return (
     <button
-      onClick={() => signIn('google', { redirect: true, callbackUrl: '/' })}
+      onClick={() => signIn('google', { redirect: true, callbackUrl: '/account' })}
       className='flex gap-3 justify-center items-center bg-white shadow text-center text-gray-700 w-full py-4'
     >
       <FontAwesomeIcon icon={faGoogle} className='h-6 w-6' />
